568560cf349a4cffbd404ebe4d05e6061bcb3359
Brian Washington <bw49078@gmail.com>
2023-02-23 08:47:24 -0600

    removed for loop

--- backend/routes/api/spots.js
+++ backend/routes/api/spots.js
@@ -10,28 +10,7 @@ router.get("/", async (req, res) => {
     const allSpots = await Spot.findAll();
     const allSpotsData = [];
 
-    for (let i = 0; i < allSpots.length; i++) {
-        let spot = allSpots[i];
-        const spotData = spot.toJSON();
-        const image = await SpotImage.findOne({
-            where: {
-                spotId: {
-                    [Op.is]: spot.id
-                }
-            }
-        })
-
-        if (image) {
-            spotData.previewImage = image.url;
-
-        } else {
-            spotData.previewImage = ""
-        }
-
-        allSpotsData.push(spotData);
-    }
-
-    res.json(allSpotsData);
+    res.json(allSpots);
 })
 
 module.exports = router;
