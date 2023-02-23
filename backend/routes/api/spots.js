c93ea7664bb60fbb7c4de5c5876c862c4ffacd59
Brian Washington <bw49078@gmail.com>
2023-02-23 13:40:46 -0600

    add current user spots endpoint

--- backend/routes/api/spots.js
+++ backend/routes/api/spots.js
@@ -55,8 +55,10 @@ router.get("/", restoreUser, async (req, res) => {
                 [sequelize.fn('AVG', sequelize.col('stars')), 'avgRating']
             ]
         })
+        let reviewAvg = ratings.toJSON().avgRating
+        if (reviewAvg) spot.avgRating = reviewAvg;
+        else spot.avgRating = "No Review Yet"
 
-        spot.avgRating = ratings.toJSON().avgRating;
 
         delete spot.SpotImages;
         delete spot.Reviews;
@@ -68,11 +70,60 @@ router.get("/", restoreUser, async (req, res) => {
 router.get(
     '/current',
     restoreUser,
-    requireAuth,
-    (req, res) => {
+    async (req, res) => {
         const { user } = req;
         if (user) {
-            return res.json("hello")
+            const userSpots = await Spot.findAll({
+                where: {
+                    ownerId: user.id
+                },
+                include: [
+                    {
+                        model: Review
+                    },
+                    {
+                        model: SpotImage
+                    }
+                ]
+            })
+            const spotData = [];
+
+            for (let i = 0; i < userSpots.length; i++) {
+                const spot = userSpots[i];
+
+                spotData.push(spot.toJSON());
+            }
+            for (let i = 0; i < spotData.length; i++) {
+                const spot = spotData[i];
+                if (spot.SpotImages.length > 0) {
+                    for (let j = 0; j < spot.SpotImages.length; j++) {
+                        const image = spot.SpotImages[j];
+
+                        if (image.preview === true) spot.previewImage = image.url;
+                    }
+                    if (!spot.previewImage) spot.previewImage = "No Preview Image";
+                } else spot.previewImage = "No Preview Image";
+
+                const ratings = await Review.findOne({
+                    where: {
+                        spotId: spot.id
+                    },
+                    attributes: [
+                        [sequelize.fn('AVG', sequelize.col('stars')), 'avgRating']
+                    ]
+                })
+                let reviewAvg = ratings.toJSON().avgRating
+                if (reviewAvg) spot.avgRating = reviewAvg;
+                else spot.avgRating = "No Review Yet"
+
+
+                delete spot.SpotImages;
+                delete spot.Reviews;
+            }
+
+            return res.json({
+                Spots: spotData
+            })
         } else return res.json({ user: null });
     }
 );
